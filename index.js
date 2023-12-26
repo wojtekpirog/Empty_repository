a8ee0a5590a3de0cf9eeb83e617eb49a1b7c34fd
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-26 21:29:45 +0100

    Create structure & styles

---
+++ index.js
@@ -0,0 +1,4 @@
+const firstBgElem = document.querySelectorAll("div.background")[0];
+console.log(firstBgElem);
+console.log(firstBgElem.clientHeight); // ClientHeight pobiera wysokość elementu HTML bez marginesów, obramowań i scrollbara
+
