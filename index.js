23dc67ec1a2c25bdf3197dd4ee8885aa19818d06
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-26 21:50:11 +0100

    Write a script to show/hide an image on button click and rotate btn icon

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const firstBgElem = document.querySelectorAll("div.background")[0];
-console.log(firstBgElem);
-console.log(firstBgElem.clientHeight); // ClientHeight pobiera wysokość elementu HTML bez marginesów, obramowań i scrollbara
+const arrowButton = document.querySelector(".arrow");
+const arrowIcon = document.querySelector(".fas");
+const firstImg = document.querySelector(".item1");
 
+const showFirstImg = () => {
+  firstImg.classList.toggle("hide");
+
+  if (firstImg.classList.contains("hide")) {
+    arrowIcon.style.transform = "rotate(180deg)";
+  } else {
+    arrowIcon.style.transform = "rotate(0deg)";
+  }
+}
+
+arrowButton.addEventListener("click", showFirstImg);
