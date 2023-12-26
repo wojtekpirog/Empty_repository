6a57aa57540f1004e9ae0ad6f3c8114c1c034f66
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-26 23:53:44 +0100

    Write a program to increase and decrease text font size on button click

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const arrowButton = document.querySelector(".arrow");
-const arrowIcon = document.querySelector(".fas");
-const firstImg = document.querySelector(".item1");
+const plusButton = document.querySelector("button:first-of-type");
+const minusButton = document.querySelector("button:nth-of-type(2)");
+const colorButton = document.querySelector("button:nth-of-type(3)");
+const paragraph = document.querySelector("p");
 
-const showFirstImg = () => {
-  firstImg.classList.toggle("hide");
+const getFontSize = () => {
+  const fontSize = getComputedStyle(paragraph).fontSize;
+  return parseFloat(fontSize); 
+}
 
-  if (firstImg.classList.contains("hide")) {
-    arrowIcon.style.transform = "rotate(180deg)";
+const zoomIn = () => {
+  let fontSize = getFontSize();
+  if (fontSize <= 30) {
+    fontSize += 2;
+    paragraph.style.fontSize = `${fontSize}px`;
   } else {
-    arrowIcon.style.transform = "rotate(0deg)";
+    return;
   }
 }
 
-arrowButton.addEventListener("click", showFirstImg);
+const zoomOut = () => {
+  let fontSize = getFontSize();
+  if (fontSize >= 18) {
+    fontSize -= 2;
+    paragraph.style.fontSize = `${fontSize}px`;
+  } else {
+    return;
+  }
+}
+
+plusButton.addEventListener("click", zoomIn);
+minusButton.addEventListener("click", zoomOut);
