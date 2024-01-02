c3d4492eb207a3022edc27d3315d887047ec5410
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-02 23:30:32 +0100

    Create a sidebar that can hide and show on btn click

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
-const minLength = document.querySelector(".minLength");
-const letters = /[a-z]/i;
-const numbers = /[0-9]/;
-const special = /[!@#$%^&*()]/;
-const minValue = 10;
+const burgerBtn = document.querySelector("button.burger");
+const barsIco = document.querySelector("i.fa-bars");
+const xIco = document.querySelector("i.fa-times");
+const nav = document.querySelector("nav ul");
 
-// RegExp jako wyrażenie regularne - poprawny adres email
-// /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i
-
-minLength.textContent = `minimum ${minValue} znaków`;
-
-const showMsg = () => {
-  if (pass.value.length === 0) {
-    p.textContent = "Nie podałeś hasła...";
-  } else {
-    // p.textContent = `Długość ciągu znaków z inputa: ${pass.value.length}`;
-    if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
-      p.textContent = "Masz bardzo dobre hasło! 💪";
-      p.style.color = "lime";
-    } else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
-      p.textContent = "Masz dobre hasło! 👍"
-      p.style.color = "gold";
-    } else {
-      p.textContent = "Masz słabe hasło! 😥"
-      p.style.color = "tomato";      
-    }
-  }
+const toggleMenu = () => {
+  nav.classList.toggle("active");
+  burgerBtn.classList.toggle("active");
+  barsIco.classList.toggle("hide");
+  xIco.classList.toggle("hide");
 }
 
-const checkPassword = () => {
-  if (pass.value === "") {
-    p.textContent = "Nie podałeś hasła...";
-    p.style.color = "tomato";
-  } else {
-    showMsg();
-  }
-}
-
-pass.addEventListener("keyup", checkPassword);
+burgerBtn.addEventListener("click", toggleMenu);
