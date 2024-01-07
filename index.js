89e476e10a4e2b26aa72cb43f9f42b15b5b45dc6
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-07 13:22:29 +0100

    Create an object and print its data

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-const converter = document.querySelector("#converter");
-const result = document.querySelector(".result");
-const convBtn = document.querySelector(".conv");
-const resetBtn = document.querySelector(".reset");
-const changeBtn = document.querySelector(".change");
-
-const letterC = document.querySelector(".letterC");
-const letterF = document.querySelector(".letterF");
-let fahrenheit;
-let celsius;
-
-// T(°F) = (T(°C) * 1.8) + 32;
-// T(°C) = (T(°F) - 32) / 1.8;
-
-const swapLetters = () => {
-  if (letterC.innerText === "°C") {
-    letterC.innerText = "°F";
-    letterF.innerText = "°C";
-    result.textContent = "";
-  } else {
-    letterC.innerText = "°C";
-    letterF.innerText = "°F";
-    result.textContent = "";
+const wojtas = {
+  firstName: "Wojciech",
+  lastName: "Piróg",
+  nickname: "Wojtas",
+  age: 23,
+  birthdate: new Date(2000, 0, 19),
+  "hair-color": "black",
+  "marital-status": "single",
+  sex: "male",
+  likesCats: true,
+  car: {
+    brand: "Citroen",
+    model: "C1",
+    yearLaunched: 2006,
+    "laquer-color": "Silver",
+    registrationInfo: {
+      numberplate: "DBL HS40"
+    }
   }
 }
 
-const convertCtoF = () => {
-  fahrenheit = (parseFloat(converter.value) * 1.8) + 32;
-  result.textContent = `${converter.value}°C = ${fahrenheit.toFixed(2)}°F`;  
+// Pętla `for(in)` działa na obiektach
+const birthdate = wojtas["birthdate"];
+
+// Opcje formatowania daty:
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric"
 }
 
-const convertFtoC = () => {
-  celsius = (parseFloat(converter.value) - 32) / 1.8;
-  result.textContent = `${converter.value}°F = ${celsius.toFixed(2)}°C`;  
+// Funkcja formatująca datę:
+const formatter = new Intl.DateTimeFormat('pl-PL', options);
+const dateReady = formatter.format(birthdate);
+
+wojtas["birthdate"] = dateReady;
+
+for (const key in wojtas) {
+  console.log(`Key: ${key} | Value: ${wojtas[key]}`);
 }
 
-const convert = () => {
-  if (!converter.value) {
-    result.style.color = "tomato";
-    result.textContent = "Nie podano wartości do konwersji!";
-  } else {
-    result.style.color = "white";
-    if (letterC.innerText === "°C") {
-      convertCtoF();
-    } else {
-      convertFtoC();
-    }    
-  }
-  converter.value = "";
-}
+// const birthdate = wojtas.birthdate;
 
-const reset = () => {
-  converter.value = "";
-  result.textContent = "";
-}
+// const formattingOptions = {
+//   weekday: "long",
+//   day: "numeric",
+//   month: "long",
+//   year: "numeric"
+// };
 
-changeBtn.addEventListener("click", swapLetters);
-convBtn.addEventListener("click", convert);
-resetBtn.addEventListener("click", reset);
+// const formatter = new Intl.DateTimeFormat('pl-PL', formattingOptions);
+// const formattedDate = formatter.format(birthdate);
+// console.log(formattedDate);
