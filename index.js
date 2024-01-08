709558d56c60012d29cfbbaef5dbf07cf479a258
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-08 20:16:50 +0100

    Create a function and some objects and bind the function with an object

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-// Konstruktor to funkcja służąca do tworzenia obiektów
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-const parag = document.querySelector(".result");
-
-function Meal(name, price) {
-  this.name = name 
-  this.price = price
+function printCarInfo() {
+  console.log(this); // Bind służy do powiązania `this` z obiektem, czyli do ustawienia kontekstu dla `this` na konkretny obiekt:
+  console.log(`Marka: ${this.brand}, model: ${this.model}, rok produkcji: ${this.yearLaunched}`);
 }
 
-// Meal.prototype.printPrice = () => console.log(`${this.name} kosztuje ${this.price}.`);
-Meal.prototype.printPrice = function() {
-  console.log(`${this.name} kosztuje ${this.price}`);
+const car1 = {
+  brand: "Audi",
+  model: "A3",
+  yearLaunched: 2006,
+  printInformation: function() {
+    const inner = () => console.log(`Car brand: ${this.brand}`);
+    inner();
+  }
 }
 
-const meal1 = new Meal("Sałatka z tuńczykiem", 15.99);
-const meal2 = new Meal("Pizza z ananasem", 39.99);
-const meal3 = new Meal("Suchy chleb", 9.99);
+const car2 = {
+  brand: "Mercedes",
+  model: "W20",
+  yearLaunched: 2000,
+  printInformation: function() {
+    const inner = () => console.log(`Car brand: ${this.brand}`);
+    inner();
+  }
+}
 
-btn1.addEventListener("click", meal1.printPrice.bind(meal1));
-btn2.addEventListener("click", meal2.printPrice.bind(meal2));
-btn3.addEventListener("click", meal3.printPrice.bind(meal3));
+const boundInfoFunction = car1.printInformation.bind(car1);
+boundInfoFunction();
+
+const boundMercInfo = car2.printInformation.bind(car2);
+boundMercInfo();
+
+// printCarInfo.bind(car1);
+const boundCarInfo = printCarInfo.bind(car1);
+boundCarInfo();
