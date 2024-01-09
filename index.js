60eeced0bcd61ce730aa34b4f2fb414ecb92ee10
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-09 23:19:51 +0100

    Create a deep copy of an object

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-function printCarInfo() {
-  console.log(this); // Bind służy do powiązania `this` z obiektem, czyli do ustawienia kontekstu dla `this` na konkretny obiekt:
-  console.log(`Marka: ${this.brand}, model: ${this.model}, rok produkcji: ${this.yearLaunched}`);
-}
-
-const car1 = {
-  brand: "Audi",
-  model: "A3",
-  yearLaunched: 2006,
-  printInformation: function() {
-    const inner = () => console.log(`Car brand: ${this.brand}`);
-    inner();
+const deepCopy = src => {
+  if (typeof src !== 'object' || src === null) {
+    return src;
   }
-}
 
-const car2 = {
-  brand: "Mercedes",
-  model: "W20",
-  yearLaunched: 2000,
-  printInformation: function() {
-    const inner = () => console.log(`Car brand: ${this.brand}`);
-    inner();
+  let srcCopy = Array.isArray(src) ? [] : {};
+
+  for (let key in src) {
+    if (src.hasOwnProperty(key)) {
+      srcCopy[key] = deepCopy(src[key]);
+    }
   }
+
+  return srcCopy;
 }
 
-const boundInfoFunction = car1.printInformation.bind(car1);
-boundInfoFunction();
+const source = {
+  type: "źródło",
+  id: 1,
+}
 
-const boundMercInfo = car2.printInformation.bind(car2);
-boundMercInfo();
-
-// printCarInfo.bind(car1);
-const boundCarInfo = printCarInfo.bind(car1);
-boundCarInfo();
+const deepCopiedSrc = deepCopy(source);
+console.log(deepCopiedSrc);
+console.log(deepCopiedSrc === source);
