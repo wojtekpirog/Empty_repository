db538b598094a17af4b7b51bf0e4d62665e8834d
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-12 17:17:25 +0100

    Learn the basics of JS promise handling

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const deepCopy = src => {
-  if (typeof src !== 'object' || src === null) {
-    return src;
+const btn = document.querySelector("button.btn-1");
+
+const dessertStock = {
+  cheeseCake: {
+    inStockAmount: 10,
+    price: 5
+  },
+  spongeCake: {
+    inStockAmount: 4,
+    price: 7
   }
+};
 
-  let srcCopy = Array.isArray(src) ? [] : {};
+const budget = 10;
 
-  for (let key in src) {
-    if (src.hasOwnProperty(key)) {
-      srcCopy[key] = deepCopy(src[key]);
+function isAvailable() {
+  return new Promise((resolve, reject) => {
+    if (dessertStock.spongeCake.inStockAmount > 0) {
+      resolve(true);
+    } else {
+      reject(false);
     }
-  }
-
-  return srcCopy;
+  });
 }
 
-const source = {
-  type: "źródło",
-  id: 1,
+function checkPrice(productAvailable) {
+  return new Promise((resolve, reject) => {
+    if (productAvailable && dessertStock.spongeCake.price < budget) {
+      resolve("Reasonable price. Purchasing...😊");
+    } else {
+      reject("Too expensive. Cancelling the order...😥");
+    }    
+  });
 }
 
-const deepCopiedSrc = deepCopy(source);
-console.log(deepCopiedSrc);
-console.log(deepCopiedSrc === source);
+function handleIsAvailable(productAvailable) {
+  return checkPrice(productAvailable);
+}
+
+function handleCheckPrice(responseMessage) {
+  return checkPrice(responseMessage);
+}
+
+function handleFailure(error) {
+  console.error(error);
+  window.alert("Obietnica nie została spełniona 😥");
+}
+
+btn.addEventListener("click", () => {
+  isAvailable()
+    .then(handleIsAvailable)
+    .then(handleCheckPrice)
+    .then(resMsg => console.log(`Response message: ${resMsg}`))
+    .catch(handleFailure)
+});
