7474a635c8f25b41b32400931e1fa0b2e693771b
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-03 20:12:02 +0100

    Practise flexbox

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 500; i++) {
+  const div = document.createElement("div");
+  div.classList.add('square', 'one');
+  document.body.append(div);
+}
+
+for (let i = 0; i < 500; i++) {
+  const div = document.createElement("div");
+  div.classList.add('square', 'two');
+  document.body.append(div);
+}
