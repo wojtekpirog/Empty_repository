cdfb37a7e0c01dc401a00d8750fe62e3de4cd524
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-17 23:34:19 +0100

    Write an application that calculates how much each person should pay for a splitted bill.

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
 let header;
 let container;
-let priceInput;
+let amount;
+let people;
+let tipSelect;
+let calculateBtn;
+let errorBox;
+let costInfo;
 
 const main = () => {
   getElements();
@@ -10,12 +15,32 @@ const main = () => {
 const getElements = () => {
   header = document.querySelector("header");
   container = document.querySelector(".container");
-  priceInput = document.querySelector("#price");
+  amount = document.querySelector("#price");
+  people = document.querySelector("#people");
+  tipSelect = document.querySelector("#tip");
+  calculateBtn = document.querySelector(".calculate");
+  errorBox = document.querySelector(".error-box");
+  costInfo = document.querySelector(".cost-info");
   container.style.height = `calc(100vh - ${header.offsetHeight}px)`;
 }
 
 const addListeners = () => {
-  priceInput.addEventListener("input", logValue);
+  // priceInput.addEventListener("input", logValue);
+  calculateBtn.addEventListener("click", calculateTip);
+}
+
+const calculateTip = () => {
+  if (!amount.value || !people.value) {
+    errorBox.textContent = "Podaj zarówno kwotę, jak i liczbę osób!";
+  } else if (people.value < 2 || people.value > 10) {
+    errorBox.textContent = "Podaj liczbę osób pomiędzy 2 a 10!";
+  } else {
+    errorBox.textContent = "";
+    const tip = amount.value * tipSelect.value;
+    const result = (parseFloat(amount.value) + tip) / people.value;
+    costInfo.style.display = "block";
+    costInfo.textContent = `Każdy z Was powinien złożyć się po ${result.toFixed(2)} zł.`;
+  }
 }
 
 const logValue = (event) => {
