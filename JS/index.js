fabee2446fc834d09bf1ae009bd3a3829efe2c8b
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-17 01:25:26 +0100

    Design an application that splits a bill

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,25 @@
-const btn = document.querySelector("button");
-const container = document.querySelector(".container");
-const template = document.querySelector(".square-temp");
+let header;
+let container;
+let priceInput;
 
-let num = 1;
-
-const addSquare = () => {
-  const square = template.content.cloneNode(true);
-  const squareNumber = square.querySelector(".square-number");
-
-  squareNumber.textContent = num;
-  container.append(square);
-  
-  num++;
+const main = () => {
+  getElements();
+  addListeners();
 }
 
-btn.addEventListener("click", addSquare);
+const getElements = () => {
+  header = document.querySelector("header");
+  container = document.querySelector(".container");
+  priceInput = document.querySelector("#price");
+  container.style.height = `calc(100vh - ${header.offsetHeight}px)`;
+}
+
+const addListeners = () => {
+  priceInput.addEventListener("input", logValue);
+}
+
+const logValue = (event) => {
+  console.log(event.target.value);
+}
+
+document.addEventListener("DOMContentLoaded", main);
