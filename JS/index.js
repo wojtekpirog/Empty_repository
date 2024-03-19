9b68d8350e1e8324e845bdce3c8ac41f9631570e
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-19 19:35:24 +0100

    Style form

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+let pageHeading;
 let username;
 let email;
 let password;
@@ -7,16 +8,23 @@ let clearButton;
 
 const main = () => {
   getElements();
+  calculateHeight();
   addListeners();
 }
 
 const getElements = () => {
+  pageHeading = document.querySelector("h1");
   username = document.querySelector("#username");
   email = document.querySelector("#email");
   password = document.querySelector("#password");
   repeatPassword = document.querySelector("#repeat-password");
   submitButton = document.querySelector("button.submit")
   clearButton = document.querySelector("button.reset")
+}
+
+const calculateHeight = () => {
+  const headingHeight = pageHeading.offsetHeight;
+  document.querySelector("main").style.height = `calc(100% - ${headingHeight}px)`;
 }
 
 const addListeners = () => {
